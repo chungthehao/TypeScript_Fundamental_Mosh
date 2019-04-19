db375cf03ca1807934cdcac00cdab0844f826217
Henry Chung <chungthehao@gmail.com>
2019-04-19 13:20:33 +0700

    get, set methods for fields

--- constructor-params-shortcut/main.js
+++ constructor-params-shortcut/main.js
@@ -6,8 +6,18 @@ var Point = /** @class */ (function () {
     Point.prototype.draw = function () {
         console.log('X: ' + this.x + ', Y: ' + this.y);
     };
+    Point.prototype.getX = function () {
+        return this.x;
+    };
+    Point.prototype.setX = function (value) {
+        if (value < 0)
+            throw new Error('x cannot be less than 0!');
+        this.x = value;
+    };
     return Point;
 }());
 // When defining an object of a custom type, need to explicitly allocate memory to it --> initialize it using a new operator
 var point = new Point(1, -2);
+var x = point.getX();
+point.setX(3);
 point.draw();
--- constructor-params-shortcut/main.ts
+++ constructor-params-shortcut/main.ts
@@ -8,9 +8,22 @@ class Point {
     draw() {
         console.log('X: ' + this.x + ', Y: ' + this.y)
     }
+
+    getX() {
+        return this.x
+    }
+
+    setX(value: number) {
+        if (value < 0)
+            throw new Error('x cannot be less than 0!')
+
+        this.x = value
+    }
 }
 
 // When defining an object of a custom type, need to explicitly allocate memory to it --> initialize it using a new operator
 let point = new Point(1, -2)
+let x = point.getX()
+point.setX(3)
 
 point.draw()
